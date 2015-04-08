d0627b526a5e59a366f388d9656c62c953d71cbd
Danihalf <danichka@gmail.com>
2015-04-08 13:12:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello from Halif!');
-});
-
-var server = app.listen(30000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
